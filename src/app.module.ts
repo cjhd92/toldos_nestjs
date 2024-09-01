bb0f6acdbe74085c25a702e073df1c91c435ecc7
cjhd92 <cjhd92@gmail.com>
2024-09-01 10:57:24 +0200

    2 commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,8 @@ import { PdfService } from './pdf/pdf.service';
 
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.cuuq5et.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`),
-            ToldosModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://cjhd92:cesar123@cluster0.cuuq5et.mongodb.net/Toldos?retryWrites=true&w=majority&appName=Cluster0'),ToldosModule],
+  //imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.cuuq5et.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`),ToldosModule],
   controllers: [AppController],
   providers: [AppService, PdfService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Asegúrate de configurar esto según tus necesidades
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
